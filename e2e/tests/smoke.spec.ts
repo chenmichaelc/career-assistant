cfad42d34b511bee7751161287f1f5ca24e1f657
Michael Chen <chenmichaelc@outlook.com>
2026-06-07 00:14:27 -0500

    CAR-77: * Restoring semantic comments to E2E test.

--- e2e/tests/smoke.spec.ts
+++ e2e/tests/smoke.spec.ts
@@ -6,14 +6,19 @@ test('Static smoke test of Default Page on Index', async ({ page }) => {
 
   await page.goto('/');
 
+  // Expect "Career Assistant" to be the page title
   await expect(page).toHaveTitle(/Career Assistant/);
+
+  // Expect "Career Assistant" to be the page title
   await expect(page.getByText('career-assistant')).toBeVisible();
 
+  // Expect all top menu items to appear on the index page
   await expect(indexPage.topMenuRolesLink).toBeVisible();
   await expect(indexPage.topMenuAddLink).toBeVisible();
   await expect(indexPage.topMenuQueryLink).toBeVisible();
   await expect(indexPage.topMenuBackupLink).toBeVisible();
 
+  // Expect key elements of Roles table header to appear on the index page
   await expect(indexPage.rolesTableHeading).toBeVisible();
   await expect(indexPage.searchButton).toBeVisible();
 });
