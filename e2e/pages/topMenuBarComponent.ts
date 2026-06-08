d40b65db0ec2588ae154b6db52048b9429ae50fc
Michael Chen <chenmichaelc@outlook.com>
2026-06-08 01:58:29 -0500

    CAR-98: * Implemented top menu bar component object.

---
+++ e2e/pages/topMenuBarComponent.ts
@@ -0,0 +1,17 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class TopMenuBarComponent {
+    readonly topMenuBarContainer:   Locator;
+    readonly rolesLink:   Locator;
+    readonly addLink:     Locator;
+    readonly queryLink:   Locator;
+    readonly backupButton: Locator;
+
+    constructor(page: Page) {
+        this.topMenuBarContainer    = page.getByTestId('menu-bar');
+        this.rolesLink              = this.topMenuBarContainer.getByRole('link',   { name: 'roles' });
+        this.addLink                = this.topMenuBarContainer.getByRole('link',   { name: 'add' });
+        this.queryLink              = this.topMenuBarContainer.getByRole('link',   { name: 'query' });
+        this.backupButton           = this.topMenuBarContainer.getByRole('button', { name: 'backup' });
+    }
+}
